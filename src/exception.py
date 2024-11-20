9367aea0a3cc4142fa5e27d6e24d7af45f463415
harish sarwar <mdharishsarwar78@gmail.com>
2024-11-20 11:42:45 +0530

    EDA

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,6 @@
 import sys
+from src.logger import logging
+
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -18,3 +20,14 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
+    
+
+## For checking purpose weither execption.py execute perfectly or not
+
+# if __name__=="__main__":
+#     try:
+#         a = 4
+#         x = a/0
+#     except Exception as e:
+#         logging.info("division by zero error")
+#         raise CustomException(e,sys)
